168fdca6246841c454e01037b42dbc643e859c1b
Dominique Demetz <ddemetz123@icloud.com>
2019-10-19 12:05:43 -0700

    added comments
    line,,,,52

--- c-sharp-array-maker.cs
+++ c-sharp-array-maker.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace arraymaker
 {
@@ -6,38 +6,56 @@ namespace arraymaker
     {
         static void Main(string[] args)
         {
-
+            //Input list
 			Console.WriteLine("Make Your Array");
 			Console.WriteLine("Example:");
 			Console.WriteLine("Yarn, Soup, High Treason");
 			string words = Console.ReadLine();
+
+            //Getting rid of extra spaces and commas
 			words = words.TrimEnd();
 			words = words.TrimEnd(',');
 			//Console.WriteLine(words);
 
+            //counting how many items are in the list
 			int count = words.Split(',').Length;
 
+            //makes the comma, space into quote, comma, quote, space
 			string betterWords = words.Replace(", ", "\", \"");
+
+            //Input language type
 			Console.WriteLine("What language?");
-                goto um;
-	    language:
+            //skips 'um'
+                goto language;
+
+        // if the language inputted isn't a language
+	    um:
 			Console.WriteLine("um... what language?");
-        um:
+
+        //taking user input for the language, trimming any spaces and downcasing
+        language:
 			string lang = Console.ReadLine();
-			lang = lang.TrimEnd();
+			lang = lang.Trim();
 			lang = lang.ToLower();
 
 
+            //if any of these languages are selected, then it will be translated
 			    if (lang == "ruby" || lang == "javascript" || lang == "c#" || lang == "swift" || lang == "java" || lang == "c++" || lang == "python" || lang == "c")
-			{ goto translate;}
+			        { goto translate;}
+
+                // if these are selected,,, sucks
                 else if (lang == "SQL" || lang == "php")
 			        { Console.WriteLine("Working on it...");
 				      return;
 			        }
-			    else
-			        { goto language;};
 
+                //if the language isn't recognized, it gets confuzzled and sad, and lets you try again! uwu xD kmn
+			    else
+			        { goto um;};
+
+        //translations available
 		translate:
+
 			if (lang == "ruby")
 			{
 				Console.WriteLine($"array_name = [\"{betterWords}\"]");
@@ -50,19 +68,16 @@ namespace arraymaker
 
 			else if (lang == "c#" || lang == "java")
 				{
-				    //int arraySize = 5;
 					Console.WriteLine($"arrayName = new string[{count}]{{\"{betterWords}\"}};");
 				}
 
 			else if (lang == "c++" || lang == "c")
 			{
-			
 				Console.WriteLine($"string arrayName[{count}] = {{\"{betterWords}\"}};");
 			}
 
 			else if (lang == "python")
 			{
-
 				Console.WriteLine($"arrayName = [\"{betterWords}\"]");
 			}
 
